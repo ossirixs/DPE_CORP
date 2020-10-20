ba61ddf9d4a664cfd95e58d6b41d710a5545d081
Leonardo Flores <nadeshkokeane@hotmail.com>
2020-10-19 23:30:51 -0500

    Initial app and git ignore

---
+++ tests/apps.py
@@ -0,0 +1,11 @@
+"""Tests application module."""
+
+from django.apps import AppConfig
+
+
+class TestsConfig(AppConfig):
+    """
+    Tests application settings.
+    """
+    name = 'tests'
+    verbose_name = 'Tests'
