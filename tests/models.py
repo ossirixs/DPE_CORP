1179b73e3689d96986326bee9d0a0ce2759198e8
Leonardo Flores <nadeshkokeane@hotmail.com>
2020-10-19 23:33:22 -0500

    Simple user model

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+  """User model."""
+  email = models.EmailField()
+  password = models.CharField()
+
+  first_name = models.CharField()
+  last_name = models.CharField()
+
